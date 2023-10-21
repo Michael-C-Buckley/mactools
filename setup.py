c223e42038381e0ddde430a7ca1f185429226e3b
Michael Buckley <michaelcbuckley@proton.me>
2023-10-21 19:29:48 -0400

    Corrected setup to httpx from requests

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'textfsm',
         'appdirs',
-        'requests'
+        'httpx'
     ],
     keywords=['python','networking','network','mac','oui','ieee']
 )
