84d77aed1b729bdbdae86eadb5bea68e7fab6759
Michael Buckley <michaelcbuckley@proton.me>
2023-11-06 10:39:50 -0500

    Updated API calls and added  as the single call endpoint

--- mactools/oui_cache/oui_api_calls.py
+++ mactools/oui_cache/oui_api_calls.py
@@ -1,10 +1,31 @@
 # OUI Cache API Calls
 
+# Python Libraries
+from json import loads
+from typing import Union
+
 # Third-Party Libraries
 from httpx import get, Response
 
+# Local Libraries
+from mactools.macaddress import MacAddress
+from mactools.oui_cache.oui_classes import OUIRecord
+
+
+def prepare_mac(func: callable, *args) -> callable:
+    """
+    Decorator for handling strings or `MacAddress` input variables
+    """
+    def wrapper(func_input: Union[str, MacAddress], *args):
+        if not isinstance(func_input, MacAddress):
+            func_input = MacAddress(func_input)
+        return func(func_input, *args)
+    return wrapper
+
 def httpx_get(resource: str, verify: bool = False, timeout: float = 10) -> Response:
-    """"""
+    """
+    Wrapper for `httpx`
+    """
     response: Response = get(resource, verify=verify, timeout=timeout)
     response.raise_for_status()
     return response
@@ -16,11 +37,27 @@ def get_oui_text(verify: bool = False) -> Response:
     ieee_oui_url = 'https://standards-oui.ieee.org/oui/oui.txt'
     return httpx_get(ieee_oui_url, verify, timeout=30)
 
-
-def vendor_oui_lookup(mac: str, verify: bool = False) -> str|None:
+@prepare_mac
+def vendor_oui_lookup(mac: Union[str, MacAddress], verify: bool = False) -> str|None:
     """
     REST request to look-up the OUI of a mac address and returns the vendor.
-    URL has a rate limit of 1/s and daily limit of 1000 calls.
+    URL has a rate limit of 2/s and daily limit of 10,000 calls.
     """
-    raise NotImplementedError('This method is not yet implemented')
-    return  httpx_get(f'http://api.macvendors.com/{mac}', verify)
+    return  httpx_get(f'https://api.maclookup.app/v2/macs/{mac.clean}/company/name', verify)
+
+@prepare_mac
+def mac_lookup_call(mac: Union[str, MacAddress], verify: bool = False):
+    """
+    REST request to get OUI info (WIP).
+    URL has a rate limit of 2/s and daily limit of 10,000 calls.
+    """
+    response = httpx_get(f'https://api.maclookup.app/v2/macs/{mac.clean}', verify)
+    payload: dict[str, str] = loads(response.text)
+
+    record_dict = {
+        'oui': payload.get('macPrefix'),
+        'vendor': payload.get('company'),
+    }
+    return OUIRecord(**record_dict)
+
+mac_lookup_call('6026aa001122')
--- tests/test_api_calls.py
+++ tests/test_api_calls.py
@@ -1,7 +1,17 @@
 from mactools.oui_cache.oui_api_calls import (
     httpx_get,
-    get_oui_text
+    get_oui_text,
+    mac_lookup_call,
+    vendor_oui_lookup,
 )
+
+from tests.test_common import (
+    TEST_IEEE_DATA,
+    MAC48,
+    SAMPLE_EUI48
+)
+
+from mactools.oui_cache.oui_classes import OUIRecord
 
 from unittest import TestCase, main
 from unittest.mock import Mock, patch
@@ -12,28 +22,68 @@ class APITests(TestCase):
     """
     Simple tests to cover the simple API calls needed
     """
+
     @classmethod
     def setUpClass(cls) -> None:
-        cls.mock_response = Mock()
+        cls.test_mac = MAC48
+        cls.test_mac_str = SAMPLE_EUI48.mac
+        cls.test_oui = TEST_IEEE_DATA.get('oui')
+        cls.test_vendor = TEST_IEEE_DATA.get('vendor')
+
+    def prepare_mock(func: callable, *args) -> callable:
+        """
+        Decorator to yield a new, local `Mock` for HTTP responses
+        """
+        def wrapper(self, *args):
+            mock_response: Mock = Mock()
+            mock_response.status_code = 200
+            return func(self, mock_response, *args)
+        return wrapper
     
-    def test_httpx_get(self):
+    def multi_test_cases(self, func: callable, expected_result, *args) -> None:
+        """
+        
+        """
+        for test_input in [self.test_mac, self.test_mac_str]:
+            result = func(test_input)
+            self.assertEqual(result, expected_result)
+    
+    @prepare_mock
+    def test_httpx_get(self, mock_response: Mock):
         """
         Tests for valid and error cases
         """
-        with patch(f'{API_DIR}.get', return_value=self.mock_response):
-            self.mock_response.status_code = 200
+        with patch(f'{API_DIR}.get', return_value=mock_response):
             test_response = httpx_get('https://www.google.com')
-            self.assertTrue(test_response, self.mock_response)
+            self.assertTrue(test_response, mock_response)
 
         with patch(f'{API_DIR}.get', side_effect=ValueError('Test Error')):
-            self.mock_response.status_code = 404
+            mock_response.status_code = 404
             with self.assertRaises(Exception):
                 test_response = httpx_get('https://www.google.com')
     
-    def test_get_oui_text(self):
-        with patch(f'{API_DIR}.httpx_get', return_value=self.mock_response):
+    @prepare_mock
+    def test_get_oui_text(self, mock_response: Mock):
+        with patch(f'{API_DIR}.httpx_get', return_value=mock_response):
             test_response = get_oui_text()
-            self.assertEqual(test_response, self.mock_response)
+            self.assertEqual(test_response, mock_response)
+
+    @prepare_mock
+    def test_mac_lookup_call(self, mock_response: Mock):
+        
+        # The JSON-schema text response has a few particulars about construction
+        mock_response.text = '{' + f'"macPrefix":"{self.test_oui}","company":"{self.test_vendor}"' + '}'
+
+        expected_result = OUIRecord(self.test_oui, self.test_vendor)
+
+        with patch(f'{API_DIR}.httpx_get', return_value=mock_response):
+            self.multi_test_cases(mac_lookup_call, expected_result)
+
+    @prepare_mock
+    def test_vendor_oui_lookup(self, mock_response: Mock):
+        mock_response = self.test_vendor
+        with patch(f'{API_DIR}.httpx_get', return_value=mock_response):
+            self.multi_test_cases(vendor_oui_lookup, self.test_vendor)
 
 
 if __name__ == '__main__':
