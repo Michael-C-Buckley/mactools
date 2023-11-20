e7927ef7d230e40337779913321f1de1311b046e
Michael Buckley <michaelcbuckley@proton.me>
2023-11-20 07:18:46 -0500

    Updated  to load the default cache much faster and added requirements file

--- mactools/macaddress.py
+++ mactools/macaddress.py
@@ -1,15 +1,14 @@
 # MacTools MAC Address Class
 
 from mactools.basemac import BaseMac, MacNotation
-from mactools.oui_cache.oui_core import OUICache, get_oui_cache
+from mactools.oui_cache.oui_core import get_oui_cache
+
+oui_cache = get_oui_cache()
 
 class MacAddress(BaseMac):
     """
     Final class that merges `OUICache` instance into the `BaseMac` for
     look-ups automatically on creation and prevents circular dependencies
     """
-    def __init__(self, mac: str | int, format: MacNotation = MacNotation.COLON,
-                 oui_cache: OUICache = None):
-        if not oui_cache:
-            oui_cache = get_oui_cache()
+    def __init__(self, mac: str | int, format: MacNotation = MacNotation.COLON):
         super().__init__(mac, format, oui_cache)
