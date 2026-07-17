c47d3ec2bd398894751275b0b551f5e962387635
Michael Buckley <michaelcbuckley@proton.me>
2026-07-16 21:08:25 -0400

    Ruff: fixed re-exports

--- mactools/__init__.py
+++ mactools/__init__.py
@@ -1,15 +1,23 @@
 # MacTools Exposed Imports
 
-from mactools.mac_common import (create_random_hex_bit,
-                                 create_random_hex_string, create_random_mac,
-                                 fill_hex, hex_range, prepare_oui)
-from mactools.macaddress import MacAddress, MacNotation
-from mactools.oui_cache.oui_classes import OUICache
-from mactools.oui_cache.oui_common import UPDATE_IEEE
-from mactools.oui_cache.oui_core import (get_oui_cache, get_oui_record,
-                                         get_oui_vendor)
-from mactools.update_ieee import update_ieee_files
-from mactools.version import __version__
+from mactools.mac_common import (
+    create_random_hex_bit as create_random_hex_bit,
+    create_random_hex_string as create_random_hex_string,
+    create_random_mac as create_random_mac,
+    fill_hex as fill_hex,
+    hex_range as hex_range,
+    prepare_oui as prepare_oui,
+)
+from mactools.macaddress import MacAddress as MacAddress, MacNotation as MacNotation
+from mactools.oui_cache.oui_classes import OUICache as OUICache
+from mactools.oui_cache.oui_common import UPDATE_IEEE as UPDATE_IEEE
+from mactools.oui_cache.oui_core import (
+    get_oui_cache as get_oui_cache,
+    get_oui_record as get_oui_record,
+    get_oui_vendor as get_oui_vendor,
+)
+from mactools.update_ieee import update_ieee_files as update_ieee_files
+from mactools.version import __version__ as __version__
 
 # Try to update IEEE files on first import if they don't exist
 try:
@@ -23,6 +31,6 @@ try:
         if os.path.isfile(os.path.join(ieee_dir, f))
     ):
         update_ieee_files(overwrite=True)
-except Exception:
+except Exception:  # nosec B110
     # If update fails, continue anyway - package can still work with cached data
     pass
--- mactools/oui_cache/__init__.py
+++ mactools/oui_cache/__init__.py
@@ -1,5 +1,5 @@
 # MacTools OUI Cache Exposed Imports
 
-from mactools.oui_cache.oui_classes import OUICache, OUIType
-from mactools.oui_cache.oui_common import UPDATE_IEEE
-from mactools.oui_cache.oui_core import get_oui_cache
+from mactools.oui_cache.oui_classes import OUICache as OUICache, OUIType as OUIType
+from mactools.oui_cache.oui_common import UPDATE_IEEE as UPDATE_IEEE
+from mactools.oui_cache.oui_core import get_oui_cache as get_oui_cache
